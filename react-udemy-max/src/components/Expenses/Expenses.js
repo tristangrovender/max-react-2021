e9b1a052da8c5062df8050e4fa445e1f96af4273
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-10-19 17:30:35 -0500

    Adding conditional return statements

--- react-udemy-max/src/components/Expenses/Expenses.js
+++ react-udemy-max/src/components/Expenses/Expenses.js
@@ -3,8 +3,8 @@ import "./Expenses.css";
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState("2019");
@@ -17,19 +17,6 @@ const Expenses = props => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(expense => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = props => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
